db776fd94aca15d4691beb1c570f8a5b6a398faf
Daniel Bony Costa Garcia <danielbony2105@gmail.com>
2025-05-14 13:50:52 -0300

    passando o conteudo do html pro json

---
+++ codigo/public/daniel-bony/app.js
@@ -0,0 +1,31 @@
+///listagem dos conteudos///
+document.addEventListener("DOMContentLoaded", () => {
+    const cursos = {
+        recomendados: [
+            {
+                titulo: "Como utilizar o instagram",
+            },
+            {
+                titulo: "Como otimizar suas vendas online",
+            }
+        ],
+        maisvistos: [
+            {
+                titulo: "Como gravar videos para o youtube",
+            },
+            {
+                titulo: "Como criar conta no instagram",
+            }
+        ],
+        redessociais: [
+            {
+                titulo: "Instagram",
+            },
+            {
+                titulo: "Facebook",
+            },
+            {
+                titulo: "Conecta rural",
+            }
+        ]
+    };
