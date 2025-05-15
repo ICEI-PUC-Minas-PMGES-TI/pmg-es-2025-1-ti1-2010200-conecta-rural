e448b4890bcc54e31786be5d0d989966aec636bd
LuizHenriqueHO <lu.luizhenriqueho@gmail.com>
2025-05-15 18:39:33 -0300

    preço

--- codigo/Luiz/Mercado/js/mercado.js
+++ codigo/Luiz/Mercado/js/mercado.js
@@ -5,14 +5,16 @@ const produtos = [
         local: "Feira do Bairro Centro",
         responsavel: "Joana Almeida",
         contato: "(11) 98765-4321",
-        descricao: "Cenouras frescas, cultivadas sem agrotóxicos. Vendidas por quilo."
+        preco: "35,00",
+        descricao: "Cenouras frescas, 1kg, cultivadas sem agrotóxicos. Vendidas por quilo.",
     },
     {
         nome: "Couve Manteiga",
         local: "Mercado Verde Vida",
         responsavel: "Lucas Rocha",
         contato: "(11) 99888-1122",
-        descricao: "Folhas verdes e crocantes, ideais para refogados e sucos detox."
+        preco: "35,00",
+        descricao: "Folhas verdes e crocantes, ideais para refogados e sucos detox.",
     }
 ];
 
@@ -29,6 +31,7 @@ descricaoDiv.innerHTML = `
         <p><strong>Local:</strong> ${produto.local}</p>
         <p><strong>Responsável:</strong> ${produto.responsavel}</p>
         <p><strong>Contato:</strong> ${produto.contato}</p>
+        <p><strong>Preço:</strong> ${produto.preco}</p>
         <p><strong>Descrição:</strong> ${produto.descricao}</p>
         <a href="#"><h3>Comprar</h3></a>
     </div>
