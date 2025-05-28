100d80c5790998abe4108b572916d63a6208a3a8
Caio César Falinacio dos Santos <caiofalinacio@gmail.com>
2025-05-28 20:01:01 -0300

    Modificação nas configurações do mapa

--- codigo/public/caio-falinacio/js/map.js
+++ codigo/public/caio-falinacio/js/map.js
@@ -1,9 +1,11 @@
 var map;
+var OpcoesMapa = {
+    center: {lat: -10.333, lng: -53.200},
+    zoom: 4.5,
+    mapTypeId: 'satellite'
+}
 const containerMapa = document.querySelector('#map');
 
 function iniciarMapa(){
-    map = new google.maps.Map(containerMapa,{
-    center: {lat: -10.333, lng: -53.200},
-    zoom: 4.5
-    })
+    map = new google.maps.Map(containerMapa, OpcoesMapa);
 }
