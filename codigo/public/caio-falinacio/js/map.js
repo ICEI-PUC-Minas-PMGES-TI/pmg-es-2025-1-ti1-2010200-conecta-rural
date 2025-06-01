17b82d3ab93134201df5ca58fb5b888a38be4d9a
Caio César Falinacio dos Santos <caiofalinacio@gmail.com>
2025-06-01 19:13:07 -0300

    Mapa funcional e marcadores adicionados na página

--- codigo/public/caio-falinacio/js/map.js
+++ codigo/public/caio-falinacio/js/map.js
@@ -1,11 +1,35 @@
-var map;
-var OpcoesMapa = {
-    center: {lat: -10.333, lng: -53.200},
-    zoom: 4.5,
-    mapTypeId: 'satellite'
-}
-const containerMapa = document.querySelector('#map');
-
 function iniciarMapa(){
-    map = new google.maps.Map(containerMapa, OpcoesMapa);
-}
+    const mapa = new google.maps.Map(document.querySelector("#map"), {
+        zoom: 6,
+        center: {lat: -18.5, lng: -44},
+    });
+    
+    const geocoder = new google.maps.Geocoder();
+
+    fetch('js/produtos.json')
+    .then(res => res.json())
+    .then(produtos => {
+        const produtoresUnicos = {};
+        produtos.forEach(produto => {
+            const produtor = produto.produtor;
+            const chave = produtor.nome + produtor.localizacao;
+            if(!produtoresUnicos[chave]){
+                produtoresUnicos[chave] = produtor;
+            }
+        });
+        Object.values(produtoresUnicos).forEach(produtor => {
+            geocoder.geocode({ address: produtor.localizacao}, (results, status) => {
+                if (status === "OK"){
+                    new google.maps.Marker({
+                        map: mapa,
+                        position: results[0].geometry.location,
+                        title: `${produtor.nome} - ${produtor.propriedade}`
+                    });
+                } else {
+                    console.error("Erro na geocodificação: ", status, produtor.localizacao);
+                }
+            })
+        })
+    })
+    .catch(error => console.error("Erro ao carregar JSON: ", error));
+} 
