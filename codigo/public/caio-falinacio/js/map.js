15ebc606622932615c334a48eae9f2ae2098e3a3
Caio César Falinacio dos Santos <caiofalinacio@gmail.com>
2025-06-04 15:44:57 -0300

    Funcionalidade da API e do filtro concluídas

--- codigo/public/caio-falinacio/js/map.js
+++ codigo/public/caio-falinacio/js/map.js
@@ -1,35 +1,50 @@
-function iniciarMapa(){
-    const mapa = new google.maps.Map(document.querySelector("#map"), {
-        zoom: 6,
-        center: {lat: -18.5, lng: -44},
-    });
-    
-    const geocoder = new google.maps.Geocoder();
-
+function iniciarMapa (){
     fetch('js/produtos.json')
     .then(res => res.json())
     .then(produtos => {
-        const produtoresUnicos = {};
-        produtos.forEach(produto => {
-            const produtor = produto.produtor;
-            const chave = produtor.nome + produtor.localizacao;
-            if(!produtoresUnicos[chave]){
-                produtoresUnicos[chave] = produtor;
-            }
+        const filtroSelect = document.querySelector('#filtroProduto');
+        const nomeProduto = [...new Set(produtos.map(p => p.nome))];
+    
+        nomeProduto.forEach(nome => {
+            const option = document.createElement('option');
+            option.value = nome;
+            option.textContent = nome;
+            filtroSelect.appendChild(option);
         });
-        Object.values(produtoresUnicos).forEach(produtor => {
-            geocoder.geocode({ address: produtor.localizacao}, (results, status) => {
-                if (status === "OK"){
-                    new google.maps.Marker({
+    
+        const marcadores = [];
+        const mapa = new google.maps.Map(document.querySelector('#map'), {
+            zoom: 6,
+            center: {lat: -18.5, lng: -44},
+        });
+    
+        const geocoder = new google.maps.Geocoder();
+    
+        produtos.forEach(produto => {
+            geocoder.geocode({ address: produto.produtor.localizacao}, (results, status) => {
+                if(status === "OK"){
+                    const marcador = new google.maps.Marker({
                         map: mapa,
                         position: results[0].geometry.location,
-                        title: `${produtor.nome} - ${produtor.propriedade}`
+                        title: `${produto.nome} - ${produto.produtor.nome}`
                     });
+                    marcador.produto = produto.nome;
+                    marcadores.push(marcador);
                 } else {
-                    console.error("Erro na geocodificação: ", status, produtor.localizacao);
+                    console.error("Erro na geocodificação: ", status, produto.produtor.localizacao);
                 }
-            })
-        })
+            });
+        });
+        filtroSelect.addEventListener('change', () => {
+            const selecionado = filtroSelect.value;
+            marcadores.forEach(marcador => {
+                if(selecionado === "Todos" || marcador.produto === selecionado){
+                    marcador.setMap(mapa);
+                } else {
+                    marcador.setMap(null);
+                }
+            });
+        });
     })
-    .catch(error => console.error("Erro ao carregar JSON: ", error));
-} 
+    .catch(error => console.error('Erro ao carregar JSON: ', error));
+}
