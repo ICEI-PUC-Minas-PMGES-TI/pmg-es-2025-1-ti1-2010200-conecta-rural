f5ea3b4d1683104b9c48d8ebf165e7efca2290ad
Caio César Falinacio dos Santos <caiofalinacio@gmail.com>
2025-06-08 15:03:37 -0300

    Mais produtos adicionados e aplicação da funcionalidade de uma janela de informações ao clicar nos marcadores

--- codigo/public/caio-falinacio/js/map.js
+++ codigo/public/caio-falinacio/js/map.js
@@ -43,6 +43,8 @@ function iniciarMapa (){
             zoom: 6,
             center: {lat: -18.5, lng: -44},
         });
+
+        const janelaInfos = new google.maps.InfoWindow();
     
         const geocoder = new google.maps.Geocoder();
     
@@ -56,6 +58,19 @@ function iniciarMapa (){
                     });
                     marcador.produto = produto.nome;
                     marcadores.push(marcador);
+
+                    marcador.addListener('click', () => {
+                        const infos = `
+                        <div style="max-width: 250px">
+                            <h3>${produto.nome}</h3>
+                            <p><strong>Produtor:</strong>${produto.produtor.nome}</p>
+                            <p><strong>Local:</strong>${produto.produtor.localizacao}</p>
+                            <p><strong>Preço:</strong>R$${produto.preco}</p>
+                            ${produto.imagens[0] ? `<img src="../assets/images/${produto.imagens[0]}" alt="${produto.nome}" style="width: 100%; height: auto; margin-top: 8px;">` : ''}
+                        `;
+                        janelaInfos.setContent(infos);
+                        janelaInfos.open(mapa, marcador);
+                    });
                 } else {
                     console.error("Erro na geocodificação: ", status, produto.produtor.localizacao);
                 }
